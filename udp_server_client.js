569fa92bace2ec2f167232c4918aef4b981df845
iaicLab at TKU 個別化智慧通訊實驗室 <iaiclab.tku@gmail.com>
2017-02-21 17:38:42 +0800

    Create udp_server_client.js

---
+++ udp_server_client.js
@@ -0,0 +1,38 @@
+
+// =======================================================================
+//       UDP server
+
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
+
+// ========================================================================
+//      UDP client
+
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var message = new Buffer('My KungFu is Good!');
+
+var client = dgram.createSocket('udp4');
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+    client.close();
+});
+
