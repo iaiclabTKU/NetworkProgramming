aadb083f7287b1ba854763c08aea3ba7a7bcc626
iaicLab at TKU 個別化智慧通訊實驗室 <iaiclab.tku@gmail.com>
2017-02-21 17:36:16 +0800

    Create tcp_server_client.js

---
+++ tcp_server_client.js
@@ -0,0 +1,76 @@
+//
+// TCP server version 1
+//
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
+
+// ================================================================
+//             TCP server version 2
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 6969;
+var server = net.createServer();
+server.listen(PORT, HOST);
+console.log('Server listening on ' + server.address().address +':'+ server.address().port);
+server.on('connection', function(sock) {
+    
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    // other stuff is the same from here
+    
+});
+// ================================================================
+//             TCP client
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+var client = new net.Socket();
+client.connect(PORT, HOST, function() {
+
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+    client.write('I am Chuck Norris!');
+
+});
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+    
+    console.log('DATA: ' + data);
+    // Close the client socket completely
+    client.destroy();
+    
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+});
